9959c5673e69d3e0d2c830d4ac6d53c00eba175a
Herman Paul Chávez <hpc__@outlook.com>
2019-04-11 12:08:03 -0400

    changed version number to 0.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='snn',
-      version='0.2',
+      version='0.3',
       description='Simple neural network for solving problems of selection',
       url='https://github.com/hpc0/simple-neural-network',
       author='Herman Paul Chávez',
