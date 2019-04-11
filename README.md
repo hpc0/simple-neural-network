c5f6f1c17b35c4365c29ef4808fca52aaa01726e
Herman Paul Chávez <hpc__@outlook.com>
2019-04-11 12:00:29 -0400

    implemented regression task

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # simple-neural-network
 
-Simple neural network for solving problems of selection.
+Simple neural network for solving problems of classification and regression.
 
 ## Installation
 
@@ -17,18 +17,33 @@ $ pip install snn
 from snn import SNN
 
 
-# Create a neural network instance
-nn = SNN(topology=[3, 4, 5, 3, 1], n_input = 2)
+# CREATE A NEURAL NETWORK INSTANCE
+# topology: List of integers representing the number of neurons in each layer.
+#           The last element is for the output layer.
+# n_input : The number of the input variables.
+# task    : 0 for regression, 1 for classification
+nn = SNN(topology=[3, 4, 5, 6, 4], n_input = 2, task=0)
 
 
-# train the neural network
+# TRAIN THE NEURAL NETWORK
 for i in range(1000):
-    nn.train(input_list=[2.45, 4.67], target=[1], lr=0.3)
+    # input_list: list of floating point numbers representing each input variable.
+    #             The number of elements must be the same as the n_input parameter used to create the network.
+    # target    : The training target for the current input_list taken from the set of data.
+    #             The number of elements must be the same as the number of elements in the last layer
+    #             (i.e. the last element of the topology parameter used to create the network).
+    # lr        : The learning rate. The factor that multiplies the derivative of the loss value with respect
+    #             to each coefficient (w) of the network to get the deltas of the w coefficients.
+    nn.train(input_list=[2.45, 4.67], target=[1.345, 3.45, -5.34, 8.54], lr=0.3)
+    
+    # The deltas are not applied (added) until this function is called. Call this after each call to SNN.train().
     nn.apply_training()
 
 
-# eval some input
-output = nn.eval([3.55, 2.73])
+# EVAL SOME INPUT
+# input_list: list of input variables to be evaluated by the network. The number of elements must be the same
+#             as the the n_input param used to create the network.
+output = nn.eval(input_list=[3.55, 2.73])
 print(output)
 ```
 
--- snn/__init__.py
+++ snn/__init__.py
@@ -2,20 +2,33 @@
 # coding: utf-8
 import numpy as np
 
+# tasks
+REGRESSION = 0
+CLASSIFICATION = 1
 
-# sigmoid function
+
+# activation functions
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 
+def identity(x):
+    return x
+
+
 class Layer:
-    def train(self, prev_z, target=np.array([]), loss=None, next_w=np.array([]), next_dL_da=np.array([]), lr=0.2):
-        if next_dL_da.size == 0:
+    def train(self, prev_z, task, target=np.array([]), loss=None, next_w=np.array([]), next_dL_da=np.array([]), lr=0.2):
+        if next_dL_da.size == 0:  # last layer
             dL_dz = (self.z - target) / loss
+            if task == REGRESSION:
+                dz_da = 1
+            else:  # task == CLASSIFICATION
+                dz_da = self.z * (1 - self.z)
         else:
             dL_dz = next_dL_da @ next_w
+            dz_da = self.z * (1 - self.z)
 
-        dz_da = self.z * (1 - self.z)
+        # dz_da = self.z * (1 - self.z)
         self.dL_da = dL_dz * dz_da
         self.dL_dw = (np.tile(prev_z, (len(self.dL_da), 1)).T * self.dL_da).T
         self.dL_db = self.dL_da
@@ -27,9 +40,9 @@ class Layer:
         self.w += self.delta_w
         self.b += self.delta_b
 
-    def eval(self, input_list):
+    def eval(self, input_list, act_f):
         a = self.w @ input_list + self.b
-        self.z = sigmoid(a)
+        self.z = act_f(a)
 
     def __init__(self, n_neurons, n_input) -> None:
         super().__init__()
@@ -51,9 +64,10 @@ class SNN:
                 prev_z = self.layers[i - 1].z
 
             if i == len(self.layers) - 1:
-                self.layers[i].train(prev_z, target, self.loss, lr=lr)
+                self.layers[i].train(prev_z, self.task, target, self.loss, lr=lr)
             else:
-                self.layers[i].train(prev_z, next_w=self.layers[i + 1].w, next_dL_da=self.layers[i + 1].dL_da, lr=lr)
+                self.layers[i].train(prev_z, self.task, next_w=self.layers[i + 1].w,
+                                     next_dL_da=self.layers[i + 1].dL_da, lr=lr)
 
     def apply_training(self):
         for layer in self.layers:
@@ -61,33 +75,23 @@ class SNN:
 
     def eval(self, input_list):
         for i, layer in enumerate(self.layers):
-            if i == 0:
-                layer.eval(input_list)
+            if self.task == REGRESSION and i == len(self.layers) - 1:  # last layer in regression task
+                act_f = identity  # sigmoid function
             else:
-                layer.eval(self.layers[i - 1].z)
+                act_f = sigmoid  # identity function
+
+            if i == 0:
+                layer.eval(input_list, act_f)
+            else:
+                layer.eval(self.layers[i - 1].z, act_f)
         return self.layers[-1].z
 
-    def __init__(self, topology, n_input) -> None:
+    def __init__(self, topology, n_input, task=REGRESSION) -> None:
         super().__init__()
+        self.task = task
         self.layers = []
         for i, n in enumerate(topology):
             if i == 0:
                 self.layers.append(Layer(n, n_input))
             else:
                 self.layers.append(Layer(n, topology[i - 1]))
-
-# CREATE A NEURAL NETWORK INSTANCE
-#
-# nn = SNN(topology=[3, 4, 5, 3, 1], n_input = 2)
-#
-#
-# TRAIN THE NEURAL NETWORK
-#
-# for i in range(1000):
-#     nn.train(input=[2.45, 4.67], target=[1], lr=0.3)
-#     nn.apply_training()
-#
-#
-# EVAL SOME INPUT
-#
-# output = nn.eval([3.55, 2.73])
